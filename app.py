d3f8cb91a3afe5abd2b7031c9b67fefceea2344b
181830171 <181830171@smail.nju.edu.cn>
2020-11-26 14:46:30 +0800

    feat: 饼状图和时间图

--- app.py
+++ app.py
@@ -78,14 +78,72 @@ def background_thread():
         else:
             print("empty")
 
+        # 饼加折现图要求的数据格式
+        # 如果要增减子列表个数，在dataset-link中也要对应增减option中series的行数
+        data1 = [
+            ['date', '10.12', '10.13', '10.14', '10.15', '10.16', '10.17', '10.18'],
+            ['Matcha Latte', 41.1, 30.4, 65.1, 53.3, 83.8, 98.7],  # 故意少放一个数据看看效果
+            ['牛奶', 86.5, 92.1, 85.7, 83.1, 73.4, 55.1, 47.9],
+            ['芝士可可', 24.1, 67.2, 79.5, 86.4, 65.2, 82.5, 63.5],
+            ['Walnut Brownie', 55.2, 67.1, 69.2, 72.4, 53.9, 39.1, 67.2],
+            ['阿华田奶昔', 45.5, 67.1, 45.2, 72.4, 53.9, 39.1, 37.9],
+        ]
+        socketio.emit('server_response',
+                      {'data': data1, 'itemName': data1[0][0]},
+                      namespace='/test1')
+
+        # 时间周期图要求的数据格式
+        # 此处data2[i][j]表示第j天，第i个时间段内的发帖数
+        timeCount = 6;  # 每天分成6个时间段
+        categoryCount = 16;  # 共展示16天
+        data2 = [
+            [20.00, 7.1, 35.5, 54, 24, 43, 21, 35, 46, 9, 27, 36, 19, 27, 47, 28],
+            [20, 17, 15, 24, 14, 23, 11, 25, 34, 19, 19, 29, 25, 20, 36, 28],
+            [14, 23, 11, 25, 34, 19, 19, 29, 20, 7, 35, 54, 24, 43, 21, 28],
+            [15, 24, 14, 23, 11, 25, 14, 23, 11, 25, 34, 19, 21, 35, 46, 28],
+            [17, 15, 24, 14, 23, 34, 19, 19, 29, 20, 7, 11, 25, 14, 23, 28],
+            [35, 54, 24, 43, 24, 14, 23, 34, 19, 23, 11, 25, 34, 19, 35, 28]
+        ]
+        socketio.emit('server_response',
+                      {'data': data2, 'timeCount': timeCount, 'categoryCount': categoryCount},
+                      namespace='/test2')
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
 
+@app.route('/test1')
+def test1():
+    return render_template('dataset-link.html', async_mode=socketio.async_mode)
+
+
+@app.route('/test2')
+def test2():
+    return render_template('active-bar-trend.html', async_mode=socketio.async_mode)
+
+
+
 @socketio.on('connect', namespace='/test')
 def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+
+
+@socketio.on('connect', namespace='/test1')
+def test1_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+
+
+@socketio.on('connect', namespace='/test2')
+def test2_connect():
     global thread
     with thread_lock:
         if thread is None:
